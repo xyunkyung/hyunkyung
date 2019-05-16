afef26b9dd6d834284fff3e1cb0cd344e376dd03
xyunkyung <50695960+xyunkyung@users.noreply.github.com>
2019-05-16 17:32:31 +0900

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -24,72 +24,65 @@ public class Calculator extends JFrame implements ActionListener {
 		display = new JTextField(35);
 		panel = new JPanel();
 		display.setText("0.0");
+		
 		panel.setLayout(new GridLayout(0, 5, 3, 3));
 		buttons = new JButton[25];
 		int index = 0;
-		for (int rows = 0; rows < 5; rows++) {
-			for (int cols = 0; cols < 5; cols++) {
+		for(int rows = 0; rows < 5; rows++) {
+			for(int cols = 0; cols < 5; cols++) {
 				buttons[index] = new JButton(labels[index]);
-				if (cols >= 3)
+				if(cols >= 3) {
 					buttons[index].setForeground(Color.red);
-				else
+				}
+				else {
 					buttons[index].setForeground(Color.blue);
+				}
 				buttons[index].setBackground(Color.yellow);
 				panel.add(buttons[index]);
-				buttons[index].addActionListener(this);
+				buttons[index].addActionListener((e)->{
+					String command = e.getActionCommand();
+					
+					if(command.charAt(0) == 'C') {
+						startOfNumber = true;
+						result = 0;
+						operator = "=";
+						display.setText("0.0");
+					}
+					else if(!command.contentEquals("1/x") && command.charAt(0) >= '0' && command.charAt(0) <= '9' || command.equals(".")){
+						if(startOfNumber){
+							display.setText(command);
+						}
+						else {
+							display.setText(display.getText() + command);
+						}
+						startOfNumber = false;
+					}
+					else {
+						if(startOfNumber) {
+							if(command.equals("-") && !command.contentEquals("1/x")) {
+								display.setText(command);
+								startOfNumber = false;
+							}
+							else {
+								operator = command;
+							}
+						}
+						else {
+							double x = Double.parseDouble(display.getText());
+							calculate(x);
+							operator = command;
+							startOfNumber = true;
+						}
+					}
+				});
 				index++;
 			}
 		}
+		
 		add(display, BorderLayout.NORTH);
 		add(panel, BorderLayout.CENTER);
 		setVisible(true);
 		pack();
-	}
-	public void actionPerformed(ActionEvent e) {
-		String command = e.getActionCommand();
-		if (command.charAt(0) == 'C') {
-			startOfNumber = true;
-			result = 0;
-			operator = "=";
-			display.setText("0.0");if(operator.equals("+")) {
-				result += n;
-			}
-			else if(operator.equals("-")) {
-				result -= n;
-			}
-			else if(operator.equals("x")) {
-				result *= n;
-			}
-			else if(operator.equals("/")) {
-				result /= n;
-			}
-			else if(operator.equals("=")) {
-				result = n;
-			}
-			else if(operator.equals("%")) {
-				result %= n;
-			}
-			else if(operator.equals("1/x")) {
-				result = 1 / n;
-			}
-				display.setText(command);
-			else
-				display.setText(display.getText() + command);
-			startOfNumber = false;
-		} else {
-			if (startOfNumber) {
-				if (command.equals(".")) {
-					display.setText(command);
-					startOfNumber = false;
-				} else
-					operator = command;
-			} else {
-				double x = Double.parseDouble(display.getText());
-				calculate(x);
-				operator = command;
-				startOfNumber = true;
-			}
-		}
 	}
 	private void calculate(double n) {
 		if(operator.equals("+")) {
